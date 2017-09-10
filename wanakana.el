4fb9fa24a0c8c0c3942030c190cc64d6913eea9e
Mamonbo <9661031@gmail.com>
2017-09-11 00:13:16 +0900

    hiraganap 関数を追加
    原作名の `is-hiragana` もaliasしてある

--- wanakana.el
+++ wanakana.el
@@ -582,5 +582,13 @@
   (japanese-hiragana mozi)
   )
 
-
+(defun wanakana-hiraganap (mozir)
+  (loop for ch in (wanakana-private-make-itrator mozir)
+	always
+	(assoc ch wanakana-to-romaji-cell-list)
+	)
+  )
+(defun wanakana-is-hiragana (mozir)
+  (wanakana-hiraganap mozir)
+  )
 (provide 'wanakana)
