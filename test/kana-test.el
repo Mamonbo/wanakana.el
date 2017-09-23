dbfa64e0d1c5bd362bbee17cf3d4dafa353a7b88
Mamonbo <9661031@gmail.com>
2017-09-23 18:26:15 +0900

    wanakana-to-kana のためのテストを追加
    emacs lisp は(基本)ダイナミックスコープであることを利用し、グローバル変数
    `use-obsolete-kana` をこの環境内で変更している

---
+++ test/kana-test.el
@@ -0,0 +1,17 @@
+(require 'wanakana-to-kana)
+
+(let ((wanakana-use-obsolete-kana nil))
+  (assert (string-equal (wanakana-to-kana "onaji BUTTSUUJI") "おなじ ブッツウジ"))
+  (assert (string-equal (wanakana-to-kana "ONAJI buttsuuji") "オナジ ぶっつうじ"))
+  (assert (string-equal (wanakana-to-kana "座禅‘zazen’スタイル") "座禅「ざぜん」スタイル"))
+  (assert (string-equal (wanakana-to-kana "batsuge-mu") "ばつげーむ"))
+  ;; 約物の変換
+  (assert (string-equal (wanakana-to-kana "!?.:/,~-‘’“”[](){}")
+			"！？。：・、〜ー「」『』［］（）｛｝"))
+  )
+
+(let
+    ((wanakana-use-obsolete-kana t))
+  (assert (string-equal (wanakana-to-kana "we") "ゑ"))  
+  )
+
