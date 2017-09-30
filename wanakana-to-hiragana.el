a7b4c66c540c7bdca83d1e5e7500d2845af497e7
Mamonbo <9661031@gmail.com>
2017-10-01 01:05:00 +0900

    wanakana-to-hiragana と wanakana-to-katakana を追加
    さほど違いも無い(ほんまか)ので2つとも実装した

---
+++ wanakana-to-hiragana.el
@@ -0,0 +1,22 @@
+(require 'wanakana-to-kana)
+(require 'wanakana-hantei)
+
+(defun wanakana-romaji-to-hiragana (mozir)
+  (wanakana-to-kana mozir t) ; 強制的にひらがなにするために
+			     ; ignore-case を立てる
+  )
+
+(defun wanakana-to-hiragana (mozir &optional pass-romaji)
+  (cond
+   (pass-romaji (wanakana-katakana-to-hiragana mozir))
+   ((wanakana-romajip mozir) (wanakana-romaji-tohiragana mozir))
+   ((wanakana-mixedp t)
+    (wanakana-romaji-to-hiragana (wanakana-katakana-to-hiragana
+				  mozir))
+    (t (wanakana-katakana-to-hiragana mozir))
+    )
+   
+   )
+  )
+
+(provide 'wanakana-to-hiragana)
